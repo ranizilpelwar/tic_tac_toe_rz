a45044bd0c60ec69012bb26f24acd746f86427c8
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 11:57:34 -0400

    Reordered properties to be grouped together

--- lib/tic_tac_toe_rz/tictactoeruby.core/Languages/yaml_reader.rb
+++ lib/tic_tac_toe_rz/tictactoeruby.core/Languages/yaml_reader.rb
@@ -5,12 +5,12 @@ require_relative '../../TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
 module TicTacToeRZ
     module YAMLReader
       def self.read_data(file_path, property)
-        file_path = File.dirname(__FILE__) + '/' + file_path
         raise NilReferenceError, "file_path" if file_path.nil?
-        raise NilReferenceError, "property" if property.nil?
         raise InvalidValueError, "file_path" if file_path == ""
-        raise InvalidValueError, "property" if property == ""
+        file_path = File.dirname(__FILE__) + '/' + file_path
         raise InvalidValueError, "file_path = #{file_path}" if !(File.exist?(file_path))
+        raise NilReferenceError, "property" if property.nil?
+        raise InvalidValueError, "property" if property == ""
         yaml_file = YAML.load_file(file_path)
         data = yaml_file[property]
         raise InvalidValueError, property.to_s if data.nil?
