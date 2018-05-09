879e00738b0a93128bda29b82ee0f001c8384a38
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 12:03:58 -0400

    Add missing yaml_writer test file

---
+++ spec/Languages/yaml_writer_spec.rb
@@ -0,0 +1,74 @@
+require_relative '../../lib/tic_tac_toe_rz/TicTacToeRuby.Core/Languages/yaml_writer.rb'
+require_relative '../../lib/tic_tac_toe_rz/TicTacToeRuby.Core/Exceptions/nil_reference_error.rb'
+require_relative '../../lib/tic_tac_toe_rz/TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
+
+RSpec.describe "a yaml writer" do
+  context "method called write_data" do
+    it "updates the yaml content when a valid file path and property name are provided" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = "test_property"
+      value = DateTime.now.to_s
+      TicTacToeRZ::YAMLWriter.write_data(file_path, property, value)
+      file_path = File.dirname(__FILE__) + '/' + "../TestFiles/testing.en.yaml"
+      yaml_file = YAML.load_file(file_path)
+      actual_value = yaml_file[property]
+      expect(actual_value).to eq(value)
+    end
+
+    it "raises a NilReferenceError when file_path is nil" do
+      file_path = nil
+      property = "test_property"
+      value = DateTime.now.to_s
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::NilReferenceError)
+    end
+
+    it "raises a NilReferenceError when property is nil" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = nil
+      value = DateTime.now.to_s
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::NilReferenceError)
+    end
+
+    it "raises a NilReferenceError when value is nil" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = "test_property"
+      value = nil
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::NilReferenceError)
+    end
+
+    it "raises an InvalidValueError when file_path is empty" do
+      file_path = ""
+      property = "test_property"
+      value = DateTime.now.to_s
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::InvalidValueError)
+    end
+
+    it "raises an InvalidValueError when property is empty" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = ""
+      value = DateTime.now.to_s
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::InvalidValueError)
+    end
+
+    it "raises an InvalidValueError when value is empty" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = "test_property"
+      value = ""
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::InvalidValueError)
+    end
+
+    it "raises an InvalidValueError when file is non-existent" do
+      file_path = "../../../../spec/TestFiles/testing1.en.yaml"
+      property = "test_property"
+      value = DateTime.now.to_s
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::InvalidValueError)
+    end
+
+    it "raises an InvalidValueError when nothing is written to the file" do
+      file_path = "../../../../spec/TestFiles/testing.en.yaml"
+      property = "test_property"
+      value = ""
+      expect{ TicTacToeRZ::YAMLWriter.write_data(file_path, property, value) }.to raise_error(TicTacToeRZ::InvalidValueError)
+    end
+  end
+end
